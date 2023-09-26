14fd09b6fe8e96ba2dd54165aaea3942695de22e
PaleHazy <austinrpg@live.com>
2023-09-26 09:49:05 -0500

    saving

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,148 +10,111 @@ import BGRain from '@/components/BGRain/BGRain';
 import { ColorProvider, useColorProvider } from 'contexts/Color';
 import { Project, ProjectsContainer } from '@/components/Projects/Project';
 import { TechIcon, TechStack } from '@/components/Techs/Techs';
-const Home: NextPage = (props: any) => {
-    const { data, status } = useSession();
-    const router = useRouter();
-    const canvasRef = useRef();
-
-    useEffect(() => {}, []);
+const LandingPage = () => {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>palehazy</title>
-                <meta
-                    name="description"
-                    content="Javascript React Professional"
+        <div className="flex flex-col min-h-screen">
+            <div className="relative min-h-screen">
+                <div className="absolute w-full h-full overflow-hidden blur-[1px]">
+                    <Image
+                        src="/images/palehazy_beautiful_sand_dunes_with_a_BLANK_background._the_sand_7e070fd8-d861-420c-ad70-7473dd865549.png"
+                        alt="Hero"
+                        layout="fill"
+                        objectFit="cover"
+                        quality={100}
+                    />
+                </div>
+                <nav className="absolute top-0 right-0 m-4">
+                    <ul className="flex space-x-4 text-white">
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="absolute flex flex-col items-center justify-center h-full w-full text-shadow">
+                    <h1 className="text-4xl sm:text-6xl md:text-8xl text-[#cbd3fa] font-bold">
+                        PaleHazy Portfolio Tour
+                    </h1>
+                    <p className="text-xl sm:text-2xl md:text-3xl text-white mt-4">
+                        Your Hero Subtitle
+                    </p>
+                </div>
+            </div>
+            <section className="bg-[#ec7d7d] py-12 min-h-screen">
+                <div className="container mx-auto px-4">
+                    <ProfileCard
+                        imageUrl="https://source.unsplash.com/random"
+                        name="Austin P is PaleHazy"
+                        description={`I was originally born in Italy, I lived there for 13 years before moving to Texas. I am a full stack developer, devops and infrastructure admin. I have been seriosuly coding for 4 years straight. I have a passion for learning new things and I am always looking for new opportunities to grow. this site will showcase some of the skill sets I have acquired over the years. I am self taught but I do have a certification from a University of Texas bootcamp. I accredit it with giving me the contacts to understand the programmer workforce and how to navigate it. However the learning and my current skill level is due to my diligence studying day in and day out`}
+                    />
+                </div>
+            </section>
+            <section className="bg-gray-100 py-12 min-h-screen">
+                <div className="container mx-auto px-4">
+                    <h1 className="">Frontend</h1>
+                    <p>
+                        My primary expertise lies in Frontend Development, where
+                        I excel as an expert in both JavaScript and CSS. My
+                        focus is on achieving pixel-perfect designs, while
+                        ensuring optimal user experience and functionality. I
+                        remain committed to following the best practices and
+                        continuously strive to stay updated with the latest
+                        open-source trends. Although being an entrepreneur and
+                        having three children limits my availability to
+                        contribute to open-source projects, I possess the
+                        capability to do so, having re-written entire libraries
+                        to cater to my clients' and my own requirements.
+                    </p>
+
+                    <h1>These are some frontend tecnologies I am an expert with, (everything typescript)</h1>
+                    <ul>
+                        <li>React</li>
+                        <li>Next.js</li>
+
+                        <li>React Three Fiber</li>
+                    </ul>
+                </div>
+            </section>
+            <section className="bg-gray-100 py-12 min-h-screen">
+                <div className="container mx-auto px-4">
+                    <h1 className="">Backend</h1>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+const ProfileCard = ({ imageUrl, name, description }: any) => {
+    return (
+        <div className="bg-white shadow-md rounded-md p-6 text-center">
+            <div className="relative w-32 h-32 mx-auto mb-4">
+                <Image
+                    src={imageUrl}
+                    alt={name}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
                 />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className={styles.main}>
-                <section className={styles.hero} style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }} id="hero">
-                    
-                    <div style={{
-                        position: "relative",
-                        width: "100%",
-                        height: "100%",
-
-                    }}>     
-                    <BGRain />               
-                    <Canvas/>
-                    </div>
-                </section>
-                <section id="oldprojects">
-                    <h2>Past Projects</h2>
-                    <ProjectsContainer>
-                        <Project
-                            name="ChainGuard Presale"
-                            url="CGPresale/index.html"
-                            description="A presale webpage, simple and mobile friendly."
-                        />
-                        <Project
-                            name="EagleEye Landing"
-                            url="EELanding/index.html"
-                            description="A landing page for a startup."
-                        />
-                        <Project
-                            name="EagleEye Dapp"
-                            url="EEDapp/index.html"
-                            description="A dapp (decentralized application)"
-                        />
-
-                        <Project
-                            name="ChainGuard Dapp"
-                            url="https://chainguard.online"
-                            description="A home page with also a dapp (decentralized application), with interactable 3d coin"
-                        />
-                    </ProjectsContainer>
-                </section>
-                <section id="tech stack">
-                    <h2>Tech stack</h2>
-                    <TechStack>
-                        <TechIcon
-                            icon="/images/file_type_html.svg"
-                            name="HTML"
-                        />
-                        <TechIcon
-                            icon="/images/file_type_js.svg"
-                            name="JavaScript"
-                        />
-                        <TechIcon icon="/images/file_type_css.svg" name="CSS" />
-                        <TechIcon
-                            icon="/images/file_type_scss.svg"
-                            name="SCSS"
-                        />
-                        <TechIcon
-                            icon="/images/file_type_node.svg"
-                            name="NodeJs"
-                        />
-                        <TechIcon
-                            icon="/images/file_type_reactts.svg"
-                            name="React"
-                        />
-                        <TechIcon
-                            icon="/images/file_type_next.svg"
-                            name="NextJs"
-                        />
-                    </TechStack>
-                </section>
-                <section className={styles.canvas} id="canvas">
-                    <button
-                        className={styles.buttonPrimary}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            signIn();
-                        }}
-                    >
-                        Sign in
-                    </button>
-                    {data ? (
-                        <h1>Welcome {data.user?.email}</h1>
-                    ) : (
-                        <h1>Welcome Stranger</h1>
-                    )}
-                    {data && (
-                        <button onClick={() => signOut()}>Sign Out</button>
-                    )}
-                    <button
-                        onClick={() => {
-                            router.push('/donate');
-                        }}
-                    >
-                        Donate
-                    </button>
-                    <h3>Too soon ? sorry..</h3>
-                    <h3>I am Austin Palazzesi, I like to go by palehazy</h3>
-                    <h3>Take a look at some websites I made for some people</h3>
-                    <h3>ALL sites here are mobile responsive</h3>
-                </section>
-            </main>
-
-            <footer className={styles.footer}>
-                <p>Footer Yee Haw</p>
-            </footer>
+            </div>
+            <h2 className="font-semibold text-2xl mb-2">{name}</h2>
+            <p className="text-gray-600">{description}</p>
         </div>
     );
 };
-const ColorChoose = () => {
-    const [s, d] = useColorProvider();
-    return (
-        <div className={styles.colorPicker}>
-            <button
-                onClick={() => {
-                    d({ type: 'SET_FILTER', payload: 'blue' });
-                }}
-            >
-                Change Color
-            </button>
-        </div>
-    );
-};
+
+const Card = ({ title, content }: any) => (
+    <div className="bg-white shadow-md rounded-md p-4">
+        <h3 className="font-semibold text-xl mb-4">{title}</h3>
+        <p>{content}</p>
+    </div>
+);
+
+export default LandingPage;
 export async function getServerSideProps(ctx: any) {
     return {
         props: {
@@ -159,4 +122,3 @@ export async function getServerSideProps(ctx: any) {
         },
     };
 }
-export default Home;
